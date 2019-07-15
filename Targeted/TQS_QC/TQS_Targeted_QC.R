5bc8fa04d8ead5469eb65bb50f7a3f3dde86324c
Regina Lionheart <regina@lionhe.art>
2019-07-15 16:31:34 -0700

    Clean up code and add overall documentation.

--- Targeted/TQS_QC/TQS_Targeted_QC.R
+++ Targeted/TQS_QC/TQS_Targeted_QC.R
@@ -1,4 +1,3 @@
-
 library(plyr)
 library(reshape2)
 library(tidyverse)
@@ -117,17 +116,15 @@ CheckSmpFragments <- function(areas.transformed) {
   return(all.samples.IR)
 }
 
-
-# TODO (rlionheart): Complete style changes.
 # TODO (kheal, aboysen): Would it be nice to have all these tables accessible for download (ie, the RT.Range.Table, Area.Table, whatever)? Helpful to see an overview of the run?
 # Alternately, I could include a section that will print a custom summary of the run with whatever is interesting.
 
-# Import files - will be changed when integrated with Shiny
+# Import files - this import format will be changed when integrated with Shiny
 input_file <- "./Targeted/TQS_QC/datafiles/ExampleSkylineOutput_TQS.csv"
 areas.raw  <- read.csv("./Targeted/TQS_QC/datafiles/ExampleSkylineOutput_TQS.csv", row.names = NULL, header = TRUE) %>% select(-X)
 master     <- read.csv("./Targeted/TQS_QC/datafiles/HILIC_MasterList_Example.csv") %>% rename(Second.Trace = X2nd.trace)
 
-## Set the parameters for the QC -----------
+# Set the parameters for the QC
 max.height <- 1.0e8
 min.height <- 1000
 area.min   <- 1000
@@ -136,15 +133,15 @@ IR.flex    <- 0.3
 blk.thresh <- 0.3
 SN.min     <- 4
 
-## ID run types
+# ID run types
 run.type <- tolower(str_extract(areas.raw$Replicate.Name, "(?<=_)[^_]+(?=_)"))
 
-## Change variables classes as needed, add Sample.Type column
-areas.transformed  <- TransformVariables(areas.raw)
-
+# Change variables classes as needed, add Sample.Type column
+areas.transformed <- TransformVariables(areas.raw)
 
 # Ion Ratio Ranges Table  ----------------------------------------------------
-# TODO (rlionheart): add explanation of what is happening to find the IR range table.
+# Find Ion Ratio by dividing the area of the quantitative trace by the area of the secondary trace. 
+# Find the minimum and maximum IR to create reference table of IR ranges.
 IR.Table <- CheckStdFragments(areas.transformed) %>%
   group_by(Precursor.Ion.Name, Std.Type) %>%
   mutate(Std.IR.Ratio = ifelse(TRUE %in% Significant.Size, (Area[Quan.Trace == TRUE] / Area[Second.Trace == TRUE]), NA)) %>%
@@ -154,7 +151,9 @@ IR.Table <- CheckStdFragments(areas.transformed) %>%
   select(Precursor.Ion.Name, IR.min, IR.max) %>%
   unique()
 
-## Retention Time  ----------------------------------------------------
+# Retention Time Table ----------------------------------------------------
+# Find the minimum and maximum Retention Times and take the average.
+# Use this as a reference table for acceptable Retention Times.
 RT.Range.Table <- areas.transformed %>%
   select(Precursor.Ion.Name, Retention.Time, Sample.Type) %>%
   filter(Sample.Type == "std") %>%
@@ -165,10 +164,9 @@ RT.Range.Table <- areas.transformed %>%
   select(-Retention.Time) %>%
   unique()
 
-
-## Blanks ---------------------------------------
-## if the area of the blank is more than blk.thresh% of the
-## area in the sample, make a flag
+# Blanks ---------------------------------------
+# Isolate the blanks in the sample and add a column 
+# with maximum blank for each Precursor ion name.
 Blank.Table <- areas.transformed %>%
   select(Precursor.Ion.Name, Area, Sample.Type) %>%
   filter(Sample.Type == "blk") %>%
@@ -177,40 +175,42 @@ Blank.Table <- areas.transformed %>%
   select(-Area) %>%
   unique()
 
-
-## Height  ---------------------------------------
+# Height  ---------------------------------------
+# Isolate all pooled and sample Heights
 Height.Table <- areas.transformed %>%
   select(Replicate.Name, Sample.Type, Precursor.Ion.Name, Precursor.Mz, Product.Mz, Height) %>%
   filter(Sample.Type %in% c("smp", "poo")) 
 
-## Area  ---------------------------------------
+# Area  ---------------------------------------
+# Isolate all pooled and sample Areas.
 Area.Table <- areas.transformed %>%
   select(Replicate.Name, Sample.Type, Precursor.Ion.Name, Area) %>%
-  filter(Sample.Type %in% c("smp", "poo")) %>%
-  mutate(area.min.Flag = ifelse((Area < area.min), "area.min.Flag", NA))
+  filter(Sample.Type %in% c("smp", "poo")) 
 
 
-## Signal to Noise  ---------------------------------------
+# Signal to Noise  ---------------------------------------
+# Isolate all pooled and sample runs. Find the Signal to Noise
+# by dividing the Background of each run by its Area.
 SN.Table <- areas.transformed %>%
   select(Replicate.Name, Sample.Type, Precursor.Ion.Name, Area, Background) %>%
   filter(Sample.Type %in% c("smp", "poo")) %>%
   mutate(Signal.to.Noise = (Area / Background)) 
 
-
-
-## Construct final output
-# TODO (rlionheart): Tables are doubling each time- why?
-
-## Join datasets  ---------------------------------------
+# Construct final output of sample and pooled runs.
 all.samples <- areas.transformed %>%
   filter(Sample.Type %in% c("smp", "poo")) 
 
-
+# Ion Ratio Flags  ---------------------------------------
+# If the Ion Ratio falls outside of the IR.Table range +/- the
+# IR.flex value, add a flag.
 IR.flags.added <- CheckSmpFragments(areas.transformed) %>%
   left_join(IR.Table, by = "Precursor.Ion.Name") %>%
   mutate(IR.Flag = ifelse((IR.Ratio < (IR.min - IR.flex) & IR.Ratio > (IR.max + IR.flex)), "IR.Flag", NA)) %>%
   select(Replicate.Name:Area, IR.Flag)
 
+# Retention Time Flags  ---------------------------------------
+# If the Retention Time is "RT.flex" further away from the RT.Reference 
+# value from the RT.Range Table, add a flag. 
 RT.flags.added <- IR.flags.added %>%
   merge(y = all.samples, all.x = TRUE) %>%
   left_join(RT.Range.Table, by = "Precursor.Ion.Name") %>%
@@ -218,36 +218,48 @@ RT.flags.added <- IR.flags.added %>%
   select(Replicate.Name:Area, IR.Flag, RT.Flag) %>%
   arrange(Precursor.Ion.Name, Product.Mz)
 
+# Blank Flags  ---------------------------------------
+# If the Area divided by the Blank.Reference value is
+# greater than the set blk.thresh value, add a flag.
 Blank.flags.added <- RT.flags.added %>%
   left_join(Blank.Table, by = "Precursor.Ion.Name") %>%
   mutate(Blank.Reference = mean(Area, na.rm = TRUE)) %>%
   mutate(blank.Flag = ifelse((Area / Blank.Reference) < blk.thresh, "blank.Flag", NA)) %>%
   select(Replicate.Name:RT.Flag, blank.Flag)
-  
+
+# Height Flags  ---------------------------------------
+# Add a height.min.flag if the Height falls below the min.height
+# value. Add an overloaded flag if the Height falls above the
+# max.height value.
 Height.flags.added <- Blank.flags.added %>%
   left_join(Height.Table, by = c("Replicate.Name", "Precursor.Ion.Name", "Precursor.Mz", "Product.Mz")) %>%
   mutate(height.min.Flag = ifelse((Height < min.height), "height.min.Flag", NA)) %>%
   mutate(overloaded.Flag = ifelse((Height > max.height), "overloaded.Flag", NA)) %>%
   select(-Height)
 
+# Area Flags  ---------------------------------------
+# If the Area is less than the area.min value, add a flag.
 Area.flags.added <- Height.flags.added %>%
   mutate(area.min.Flag = ifelse((Area < area.min), "area.min.Flag", NA)) %>%
   select(-Sample.Type)
 
+# Signal to Noise Flags  ---------------------------------------
+# If the Signal to Noise ratio is less than the SN.min, add a flag.
 SN.flags.added <- Area.flags.added %>%
   left_join(SN.Table, by = c("Replicate.Name", "Precursor.Ion.Name", "Area")) %>%
   mutate(SN.Flag = ifelse((Signal.to.Noise < SN.min), "SN.Flag", NA)) %>%
   select(-c(Sample.Type:Signal.to.Noise))
 
-
-
-####
+# All Flags  ---------------------------------------
+# Add a column with all flags from the previous steps. 
 final.table <- SN.flags.added %>%
-  mutate(all.Flags      = paste(RT.Flag, blank.Flag, height.min.Flag, overloaded.Flag, area.min.Flag, SN.Flag, sep = ", ")) %>%
-  mutate(all.Flags      = as.character(all.Flags %>% str_remove_all("NA, ") %>%  str_remove_all("NA")))
+  mutate(all.Flags = paste(IR.Flag, RT.Flag, blank.Flag, height.min.Flag, overloaded.Flag, area.min.Flag, SN.Flag, sep = ", ")) %>%
+  mutate(all.Flags = as.character(all.Flags %>% str_remove_all("NA, ") %>%  str_remove_all("NA")))
 
 
-# If there are any standards, add those to the bottom of the dataset.
+# Standards addition  ---------------------------------------
+# Ensure there are standards in the run. Add those standards
+# back into the final table.
 Stds.test <- grepl("_Std_", areas.raw$Replicate.Name)
 
 if (any(Stds.test == TRUE)) {
@@ -258,8 +270,10 @@ if (any(Stds.test == TRUE)) {
   print("No standards exist in this set.")
 }
 
-
-# Print to file with comments and new name!`    `
+# Rename and save  ---------------------------------------
+# Add comments restating the given QC parameters. Save to 
+# current working directory with a new name, 
+# "TQSQC_<original file name>.csv
 con <- file(paste("TQSQC_", basename(input_file), sep = ""), open = "wt")
 writeLines(paste("Hello! Welcome to the world of TQS Quality Control! ",
                  "Minimum height for a real peak: ", min.height, ". ",
